dce865ac03a20a69e0795eb0eb4e476fd5b9d1da
Yu Ohno <kantillo0683@gmail.com>
2026-01-17 08:26:59 +0900

    change font

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -30,7 +30,7 @@ export function Combobox({
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
-        <Button variant="outline" role="combobox" aria-expanded={open} className={cn("justify-between", className)}>
+        <Button variant="outline" role="combobox" aria-expanded={open} className={cn("justify-between font-normal", className)}>
           {value || placeholder}
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
